db880137576862262a73ca104c62074ded83a182
Jahzeel-bit <mkjhzlbjnd@gmail.com>
2026-03-31 08:26:31 +0000

    Done

--- Randoom.java
+++ Randoom.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-Import java.util.Random;
+import java.util.Random;
 class Randoom{
     public static void main(String[] args){
         Random rand = new Random();
         int length = 32;
-        int max =126;
-        Scanner sc = new Scanner();
-        System.out.println("enter number:" );
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:" );
+        int input = sc.nextInt();
+        sc.close();
+        for (int i=0; i<input; i++){
+             int asciiVal = length+ rand.nextInt(95);
+             char result = (char) asciiVal;   
+       System.out.print(result);
+        }
     }
 }
