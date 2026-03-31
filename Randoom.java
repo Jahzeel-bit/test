deca58248b7f632ed7d31f25fa4350e7661c6a60
Jahzeel-bit <mkjhzlbjnd@gmail.com>
2026-03-31 08:06:36 +0000

    some

---
+++ Randoom.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+Import java.util.Random;
+class Randoom{
+    public static void main(String[] args){
+        Random rand = new Random();
+        int length = 32;
+        int max =126;
+        Scanner sc = new Scanner();
+        System.out.println("enter number:" );
+
+    }
+}
